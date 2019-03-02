5f26171ea8ee241f0ee092ca125484be9f95408c
ted <ted10401@gmail.com>
2019-03-02 17:19:39 +0800

    Add the battle state.

---
+++ Assets/Modules/MacanimFSM/Scripts/States/BattleState.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MacanimFSM
+{
+    public class BattleState : BaseState
+    {
+        protected override void OnStateEntered()
+        {
+            m_brain.navMeshAgent.SetDestination(m_brain.transform.position);
+        }
+    }
+}
